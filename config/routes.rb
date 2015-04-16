3af4f1f802207fc141f3334e6582de93b49bac97
Naveen <none.wiseguy@gmail.com>
2015-04-16 19:54:20 +0530

    Authentication done !

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
  root 'home#index'
+ get '/login' => 'home#login'
  get '/about' => 'home#about'
+ get '/logout' => 'home#logout'
+ post '/Auth' => 'home#authenticate'
  resources :messages, only: [:index,:new,:create]
 
   # You can have the root of your site routed with "root"
