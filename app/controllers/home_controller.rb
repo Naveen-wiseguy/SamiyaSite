63cec4a07a8090ecbb8e5761d36e9f257fce2063
Naveen <none.wiseguy@gmail.com>
2015-04-28 10:45:10 +0530

    Added a page for login redirection with our message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
   if params[:a1].casecmp("tripod")==0 && (params[:a2].casecmp("crocodile")==0 || params[:a2].casecmp("croc")==0) && params[:a3].casecmp("DPRK")==0
    session[:loggedin]=true
    flash[:success]="Logged in ! :)"
-   redirect_to root_path
+   redirect_to '/loggedin'
   else
    flash[:danger]="Incorrect"
    redirect_to '/login'
@@ -27,5 +27,11 @@ class HomeController < ApplicationController
  end
 
  def gallery
+ end
+
+ def logged
+  if session[:loggedin]==false
+    redirect_to root_path
+  end
  end
 end
